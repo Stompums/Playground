2c76066250634ff0d7bb60cce2fc30b6409c47a2
Jacob <jacobwaynejohnson@gmail.com>
2014-06-27 07:08:06 +0000

    My solution to Project Euler 12 and the question posited by Richard.

---
+++ P1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""
+What's the sum of the last ten digits of the product of the set of primes less than ten thousand?
+"""
+
+import divisor
+
+prime_king = divisor.primes(10000)
+product = 1
+for i in prime_king:
+	product = product*i
+	if product >10000000000:
+		product = product%10000000000
+
+print sum([int(business) for business in str(product)])
